c41fab15dd1f68abf28bf319e0ee9f99abe595de
Wan-Chin Chrispat <wanchinchrispat401@gmail.com>
2024-08-20 02:09:56 +0100

    remove props for footer component

--- src/Scenes/footer/index.tsx
+++ src/Scenes/footer/index.tsx
@@ -4,7 +4,7 @@ interface Props {
     setSelectedPage: (value:SelectedPage) => void
 }
 
-const Footer = ({setSelectedPage}: Props) => {
+const Footer = ({}: Props) => {
     return (
        <footer className="bg-primary-100 py-16">
         <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
